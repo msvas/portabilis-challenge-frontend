c20e626ed2a3889a19dafd827491fdc58fcf1847
Marcelo <msouvas@gmail.com>
2022-07-06 14:39:15 -0300

    bases métodos de exluir e suspender

--- pages/lista.vue
+++ pages/lista.vue
@@ -9,7 +9,20 @@
       <div class="card-content">
         <div class="card-body pt-1">
           <div>
-            <b-table striped hover :items="users" :fields="fields"></b-table>
+            <b-table striped hover :items="users" :fields="isAdmin ? fieldsAdmin : fieldsRegular">
+              <template #cell(remove_user)="data">
+                <b-button size="sm" @click="deleteUser(data.item.id)" class="mr-2">
+                  Remover
+                </b-button>
+              </template>
+              <template #cell(suspend_user)="data">
+                <b-button size="sm" @click="suspendUser(data.item.id)" class="mr-2">
+                  <span v-if="data.item.status == 'Active'">Suspender</span>
+                  <span v-else>Ativar</span>
+                </b-button>
+              </template>
+
+            </b-table>
           </div>
         </div>
       </div>
@@ -26,7 +39,31 @@ export default {
   data() {
     return {
       users: [],
-      fields: [
+      fieldsRegular: [
+        {
+          key: 'id',
+          sortable: false
+        },
+        {
+          key: 'name',
+          sortable: true,
+          label: 'Nome'
+        },
+        {
+          key: 'email',
+          sortable: true,
+        },
+        {
+          key: 'status',
+          sortable: false,
+        },
+        {
+          key: 'role',
+          sortable: true,
+          label: 'Nível de acesso'
+        },
+      ],
+      fieldsAdmin: [
           {
             key: 'id',
             sortable: false
@@ -44,19 +81,39 @@ export default {
             key: 'role',
             sortable: true,
             label: 'Nível de acesso'
+          },
+          {
+            key: 'status',
+            sortable: false,
+          },
+          {
+            key: 'remove_user',
+            label: 'Remover usuário'
+          },
+          {
+            key: 'suspend_user',
+            label: 'Suspender/Ativar'
           }
         ],
     };
   },
   mounted() {
     this.$axios.$get('api/v1/users').then((response) => {
-      console.log('-----')
-      console.log(response)
       this.users = response
     })
   },
+  computed: {
+    isAdmin() {
+      return true
+    }
+  },
   methods: {
+    deleteUser(id) {
 
+    },
+    suspendUser(id) {
+      
+    }
   }
 }
 </script>
