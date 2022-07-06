6ec530c9cc9b5f29c4891e13bfd1e235cf761186
Marcelo <msouvas@gmail.com>
2022-07-06 12:54:23 -0300

    configura tabela de exibição

--- pages/lista.vue
+++ pages/lista.vue
@@ -9,7 +9,7 @@
       <div class="card-content">
         <div class="card-body pt-1">
           <div>
-            <b-table striped hover :items="users"></b-table>
+            <b-table striped hover :items="users" :fields="fields"></b-table>
           </div>
         </div>
       </div>
@@ -26,11 +26,33 @@ export default {
   data() {
     return {
       users: [],
+      fields: [
+          {
+            key: 'id',
+            sortable: false
+          },
+          {
+            key: 'name',
+            sortable: true,
+            label: 'Nome'
+          },
+          {
+            key: 'email',
+            sortable: true,
+          },
+          {
+            key: 'role',
+            sortable: true,
+            label: 'Nível de acesso'
+          }
+        ],
     };
   },
   mounted() {
     this.$axios.$get('api/v1/users').then((response) => {
+      console.log('-----')
       console.log(response)
+      this.users = response
     })
   },
   methods: {
